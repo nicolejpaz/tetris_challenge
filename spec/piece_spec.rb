8fe4a5194c44d59e15c0c5effca90bd741ea41f0
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2014-01-15 15:57:28 -0800

    Added test for create_block method for Piece.

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -10,4 +10,10 @@ describe Piece do
       @piece.should be_an_instance_of Piece
     end
   end
+
+  context "create_block" do
+    it "creates a single black square representing the blocks that create the Tetris piece" do
+      @piece.create_block.should eql "\u2588"
+    end
+  end
 end
