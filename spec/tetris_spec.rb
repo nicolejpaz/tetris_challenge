f69abfaef0884b26cda896da4a03d3fe51b6b48e
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2014-01-15 15:41:56 -0800

    Created initial Rspec file.

---
+++ spec/tetris_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../tetris'
+
+describe Tetris do
+  context "#create_piece" do
+    it "displays the correct Unicode character for each block"
+
+    it "creates a square Tetris piece"
+  end
+end
