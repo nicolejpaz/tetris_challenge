0ef268f189335201a39a657bb0cfcbaae597f37f
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2014-01-15 15:45:39 -0800

    Added test to check if Tetris.new creates a new Tetris object.

--- spec/tetris_spec.rb
+++ spec/tetris_spec.rb
@@ -1,6 +1,16 @@
 require_relative '../tetris'
 
 describe Tetris do
+  before :each do
+    @tetris = Tetris.new 
+  end
+
+  context "#new" do
+    it "creates a new Tetris object" do
+      @tetris.should be_an_instance_of Tetris
+    end
+  end
+
   context "#create_piece" do
     it "displays the correct Unicode character for each block"
 
