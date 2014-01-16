3e4eff9aecee4be2bf1a15310743bd7d36af80e0
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2014-01-15 19:04:14 -0800

    Wrote code to reflect addition of next_piece and rows.

--- tetris.rb
+++ tetris.rb
@@ -1,2 +1,30 @@
+require_relative 'piece'
+
 class Tetris
+  BOARD_HEIGHT = 12
+  BOARD_WIDTH = 8
+
+  attr_reader :current_piece, :rows
+
+  def initialize
+    @rows = []
+    create_board_rows
+    create_board_columns
+
+    next_piece
+  end
+
+  def create_board_rows
+    BOARD_HEIGHT.times { @rows << [] }
+  end
+
+  def create_board_columns
+    @rows.each do |row|
+      BOARD_WIDTH.times { row << [] }
+    end
+  end
+
+  def next_piece
+    @current_piece = Piece.new
+  end
 end
