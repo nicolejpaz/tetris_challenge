b3b2bc20e6ddf5d1a749b8106418de3eb7137736
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2014-01-15 19:02:30 -0800

    Wrote test to check for correct board height and width.

--- spec/tetris_spec.rb
+++ spec/tetris_spec.rb
@@ -10,4 +10,21 @@ describe Tetris do
       @tetris.should be_an_instance_of Tetris
     end
   end
+
+  context "#next_piece" do
+    it "creates a new instance of Piece and assigns it to @current_piece" do
+      @tetris.next_piece
+      @tetris.current_piece.should be_an_instance_of Piece
+    end
+  end
+
+  context "#rows" do
+    it "reflects the specified constant BOARD_HEIGHT" do
+      @tetris.rows.length.should eql 12
+    end
+
+    it "reflects the specified constant BOARD_WIDTH" do
+      @tetris.rows.first.length.should eql 8
+    end
+  end
 end
